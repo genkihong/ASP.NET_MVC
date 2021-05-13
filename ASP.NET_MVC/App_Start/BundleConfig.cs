e00269806b3185b85d6af1f28da78f7a858e35af
Charles Hong <genkihong@gmail.com>
2021-05-13 13:49:28 +0800

    加入專案檔案。

---
+++ ASP.NET_MVC/App_Start/BundleConfig.cs
@@ -0,0 +1,43 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace ASP.NET_MVC
+{
+    public class BundleConfig
+    {
+        // 如需統合的詳細資訊，請瀏覽 https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        //"~/Scripts/jquery-{version}.js",                        
+                        "~/Scripts/jquery.js",                        
+                        "~/Scripts/jquery.colorbox.js",
+                        "~/Scripts/jquery.counterup.min.js",
+                        "~/Scripts/owl.carousel.min.js",
+                        "~/Scripts/waypoints.min.js",
+                        "~/Scripts/isotope.js",
+                        "~/Scripts/ini.isotope.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // 使用開發版本的 Modernizr 進行開發並學習。然後，當您
+            // 準備好可進行生產時，請使用 https://modernizr.com 的建置工具，只挑選您需要的測試。
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/style1.css",
+                      "~/Content/responsive.css",
+                      "~/Content/font-awesome.min.css",
+                      "~/Content/animate.css",
+                      "~/Content/owl.carousel.css",
+                      "~/Content/owl.theme.css",
+                      "~/Content/colorbox.css"));
+        }
+    }
+}
---
+++ ASP.NET_MVC/App_Start/RouteConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ASP.NET_MVC
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "News",
+                url: "news",
+                defaults: new { controller = "Home", action = "News" }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+        }
+    }
+}
